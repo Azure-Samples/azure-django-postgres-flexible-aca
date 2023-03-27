9c0e8af2fd29e3603f36d02d7b5dda4f2a216086
Pamela Fox <pamela.fox@gmail.com>
2023-03-27 12:25:05 -0700

    More alien fixes

--- demo-code/entrypoint.sh
+++ demo-code/entrypoint.sh
@@ -3,13 +3,12 @@
 set -e
 
 echo "${0}: running migrations."
-python manage.py migrate --settings=project.settings
-python manage.py loaddata --settings=project.settings seed_data.json
+python manage.py migrate
+python manage.py loaddata seed_data.json
+python manage.py collectstatic
 gunicorn project.wsgi:application \
-    --env DJANGO_SETTINGS_MODULE=project.settings\
     --name relecloud \
     --bind 0.0.0.0:8000 \
     --timeout 600 \
     --workers 4 \
-    --log-level=info \
-    --reload
+    --log-level=info

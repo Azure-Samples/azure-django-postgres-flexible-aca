ace84b031bcc02f398507a476ccd5abdae1d8d01
Pamela Fox <pamela.fox@gmail.com>
2023-04-09 19:30:01 +0000

    Formatting changes

--- demo-code/relecloud/playwright.py
+++ demo-code/relecloud/playwright.py
@@ -1,7 +1,8 @@
 import os
 
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase 
-from playwright.sync_api import sync_playwright, Page, expect
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from playwright.sync_api import expect, sync_playwright
+
 
 class DemoCodeTests(StaticLiveServerTestCase):
     fixtures = ["seed_data.json"]
@@ -33,15 +34,17 @@ class DemoCodeTests(StaticLiveServerTestCase):
         page.get_by_label("Notes*").fill("more mars rovers pls")
         page.get_by_role("button", name="Save").click()
         page.get_by_role("alert").click()
-        expect(page.locator("[role=alert]")).to_contain_text("Thank you, pamela! We will email you when we have more information about The Sun")
+        expect(page.locator("[role=alert]")).to_contain_text(
+            "Thank you, pamela! We will email you when we have more information about The Sun"
+        )
         page.close()
 
     def test_login(self):
         page = self.browser.new_page()
         page.goto(f"{self.live_server_url}/admin/")
-        page.wait_for_selector('text=Django administration')
-        page.fill('[name=username]', 'myuser')
-        page.fill('[name=password]', 'secret')
-        page.click('text=Log in')
+        page.wait_for_selector("text=Django administration")
+        page.fill("[name=username]", "myuser")
+        page.fill("[name=password]", "secret")
+        page.click("text=Log in")
         assert len(page.eval_on_selector(".errornote", "el => el.innerText")) > 0
         page.close()

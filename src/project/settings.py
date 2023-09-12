d65e82d6910b0a8229dd62e56e92f8e153b0806a
Pamela Fox <pamela.fox@gmail.com>
2023-09-12 19:56:06 +0000

    Update formatting

--- src/project/settings.py
+++ src/project/settings.py
@@ -70,7 +70,7 @@ ROOT_URLCONF = "project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [ BASE_DIR / "templates"],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [

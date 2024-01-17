a08bbf720fe4cb096d2b5daf831c2972f08a7154
Jay Miller <kjaymiller@gmail.com>
2024-01-17 11:12:23 -0500

    Cruft Update

--- src/project/wsgi.py
+++ src/project/wsgi.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 
 import os
 
+from azure.monitor.opentelemetry import configure_azure_monitor
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
-
+configure_azure_monitor()
 application = get_wsgi_application()
